0f27c64492033907806efcb682ac281ae61bbf8b
karan lakharwal <karanlakharwal@gmail.com>
2026-05-06 15:23:53 +1000

    invoke-backend-api-from-react-ui

--- store/src/main/java/com/portfolio/store/controller/ProductController.java
+++ store/src/main/java/com/portfolio/store/controller/ProductController.java
@@ -7,6 +7,7 @@ import com.portfolio.store.repository.ProductRepository;
 import com.portfolio.store.service.IProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +17,7 @@ import java.util.List;
 @RestController
 @RequestMapping("api/v1/products")
 @RequiredArgsConstructor
+//@CrossOrigin(origins = "http://localhost:5173")
 @JsonPropertyOrder({"productId","name","description","price","popularity","imageUrl","createdAt"})
 public class ProductController {
 
