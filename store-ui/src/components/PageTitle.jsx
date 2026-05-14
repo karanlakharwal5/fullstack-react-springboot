7dc34ce2a023ed685b3a7698a32adf17b99e3989
karan lakharwal <karanlakharwal@gmail.com>
2026-05-14 23:22:21 +1000

    added dark mode to frontend

--- store-ui/src/components/PageTitle.jsx
+++ store-ui/src/components/PageTitle.jsx
@@ -2,8 +2,6 @@ import React from "react";
 
 export default function PageTitle({ title }) {
   return (
-    <h1 className="text-3xl text-primary font-primary text-center font-extrabold mt-4 py-2">
-      {title}
-    </h1>
+    <h1 className="text-3xl text-primary dark:text-light font-primary text-center font-extrabold mt-4 py-2">{title}</h1>
   );
 }
--- store-ui/src/components/footer/Footer.jsx
+++ store-ui/src/components/footer/Footer.jsx
@@ -3,19 +3,14 @@ import { faHeart } from "@fortawesome/free-solid-svg-icons";
 
 export default function Footer() {
   return (
-    <footer className="flex justify-center items-center py-4 font-primary text-gray-700">
-      Built with{" "}
-      <FontAwesomeIcon
-        icon={faHeart}
-        className="text-red-600 mx-1 animate-pulse"
-        aria-hidden="true"
-      />
+    <footer className="flex justify-center items-center py-4 font-primary text-gray-700 dark:text-gray-300">
+      Built with <FontAwesomeIcon icon={faHeart} className="text-red-600 mx-1 animate-pulse" aria-hidden="true" />
       by
       <a
         href="https://github.com/karanlakharwal5"
         target="_blank"
         rel="noreferrer"
-        className="text-primary font-semibold px-1 transition-colors duration-300 hover:text-dark"
+        className="text-primary dark:text-lighter font-semibold px-1 transition-colors duration-300 hover:text-dark"
       >
         Karan
       </a>
